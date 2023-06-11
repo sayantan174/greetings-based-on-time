22529702fbe89babd96599170bbfaa8584ce7b0b
sayantan174 <sayantanmandal174@gmail.com>
2023-06-11 20:44:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+let greeting = "";
+const date = new Date();
+const cssStyle = {};
+if (date.getHours() >= 1 && date.getHours() <= 11) {
+  greeting = "Good Morning";
+  cssStyle.color = "green";
+} else if (date.getHours() >= 12 && date.getHours() <= 19) {
+  greeting = "Good Afternoon";
+  cssStyle.color = "orange";
+} else {
+  greeting = "Good Night";
+  cssStyle.color = "black";
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div
+        style={{
+          height: "100vh",
+          width: "100vw",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor:"#aee9e1"
+        }}
+      >
+        <h1
+          style={{
+            backgroundColor: "#ffe8c5",
+            padding: "1rem",
+            borderRadius: ".5rem",
+            color: "#ff9b8e",
+            fontSize:"4rem",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Hello Sir, <span style={{ color: cssStyle.color }}>{greeting}</span>
+        </h1>
+      </div>
+    </>
   );
 }
 
